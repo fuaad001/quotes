195312a5b173b490752111d36535cdd38131e13e
fuaad001 <husseinfuaad@hotmail.com>
2019-03-24 19:35:32 +0300

    Customize pipe to count days

---
+++ src/app/date-counter.pipe.ts
@@ -0,0 +1,27 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'dateCounter'
+})
+export class DateCounterPipe implements PipeTransform {
+
+
+  transform(value: any): number {
+    let today:any = new Date();
+
+    var difference =Math.abs(today-value)
+    const secondsPerDay= 86400;
+
+    var dateDifferenceSeconds=difference*0.001;
+
+    var dateCount = Math.round(dateDifferenceSeconds/secondsPerDay);
+
+
+    if (dateCount >= 1){
+        return dateCount - 1;
+    }else{
+        return 0;
+    }
+  }
+
+}
