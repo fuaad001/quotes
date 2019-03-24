68fac7a7e2471b3d3a8a52d979b01adc2735c542
fuaad001 <husseinfuaad@hotmail.com>
2019-03-24 19:40:48 +0300

    Add constructor code

---
+++ src/app/quote.ts
@@ -0,0 +1,3 @@
+export class Quote {
+    constructor(public id: number, public quote: string, public author: string, public name: string, public date: Date, public upVotes: number, public downVotes: number){};
+}
