f01650aae8947e23387953ac008384998ee50952
fuaad001 <husseinfuaad@hotmail.com>
2019-03-24 19:39:11 +0300

    Create display component and link other components

---
+++ src/app/quote-display/quote-display.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote'
+import { DateCounterPipe } from '../date-counter.pipe'
+
+@Component({
+  selector: 'app-quote-display',
+  templateUrl: './quote-display.component.html',
+  styleUrls: ['./quote-display.component.css']
+})
+export class QuoteDisplayComponent implements OnInit {
+
+  quotes = [
+    new Quote(1,'A dead clock is right twice a day', 'Thomas Edison', 'Hussein Fuaad', new Date(2019, 2, 22), 0, 0),
+    new Quote(2,'You never hate the road until you are missing home', 'Passenger', 'Ali Hamid', new Date(2019, 2, 23), 0, 0)
+  ]
+
+   addNewQuote(quote){
+     let quoteLength = this.quotes.length;
+     quote.id = quoteLength + 1;
+     quote.author = quote.author;
+     quote.name = quote.name
+     quote.date = new Date(quote.date)
+     this.quotes.push(quote)
+   }
+
+  deleteQuote(isComplete, index){
+    if(isComplete){
+      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].quote}`)
+
+      if(toDelete){
+        this.quotes.splice(index, 1)
+      }
+    }
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
