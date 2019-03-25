2eaf845c1f59571723bbdd411f4b4544f3e86956
fuaad001 <husseinfuaad@hotmail.com>
2019-03-25 10:15:41 +0300

    remove unneccessary componenet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { FormComponent } from './form/form.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { QuoteDisplayComponent } from './quote-display/quote-display.component';
 import { DateCounterPipe } from './date-counter.pipe';
-import { HighestQuoteComponent } from './highest-quote/highest-quote.component';
+
 
 @NgModule({
   declarations: [
@@ -18,8 +18,7 @@ import { HighestQuoteComponent } from './highest-quote/highest-quote.component';
     FormComponent,
     QuoteDetailsComponent,
     QuoteDisplayComponent,
-    DateCounterPipe,
-    HighestQuoteComponent
+    DateCounterPipe
   ],
   imports: [
     BrowserModule,
